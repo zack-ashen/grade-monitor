aefbcee9ea888a27bb01ab04f9d67cdcda1ecc77
Zachary Ashen <zachary.h.a@gmail.com>
2021-01-09 15:10:52 -0500

    home works on mobile

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,60 @@
 import './App.css';
 import './Blurb.css';
 import Login from './Login.js';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+    const width = useWindowSize().width;
+    console.log(width);
+    return (
+        <div className="App">
+            {/* Desktop */}
+            {width > 450 && <>
+            <h1 className="blurbHeader">Grade Monitor</h1>
+            <div className="blurb">
+                <p className="blurb-text">Welcome to Grade Monitor. Enter your classes weighting and your assignments to dynamically see your current grade in the class and save it. You can come back throughout the semester to update your grades and see your current standing.</p>
+                <Login />
+            </div>
+            </>}
 
-  return (
-      <div className="App">
-          <h1>Grade Book</h1>
-          <div className="blurb" id="signin-blurb">
-              <p className="blurb-text" id="signin-blurb-text">Welcome to Grade Monitor. Enter your classes weighting and your assignments to dynamically see your current grade in the class and save it. You can come back throughout the semester to update your grades and see your current standing.</p>
-              <Login />
-          </div>
-      </div>
-  );
+            {/* Mobile */}
+            {width <= 450 && <>
+            <div className="blurb">
+                <h1 className="blurbHeader">Grade Monitor</h1>
+                <p className="blurb-text">Login to start tracking your grades.</p>
+            </div>
+            <Login />
+            </>}
+
+        </div>
+    );
 }
+
+function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+  
+    useEffect(() => {
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+      
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+      
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+      
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+  
+    return windowSize;
+  }
